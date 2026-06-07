790d36a9094c2fc01d8c9fca48376def09409027
Cristian Andrés Romero <117885676+CristianRomero30@users.noreply.github.com>
2026-06-07 00:22:00 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -39,7 +39,8 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    dateStrings: true
 });
 
 // Wrapper temporal para facilitar migración a MySQL
