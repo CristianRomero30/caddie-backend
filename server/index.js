3bfc9ee7f57133c34951cde5fa02ac22ee16d437
Cristian Andrés Romero <117885676+CristianRomero30@users.noreply.github.com>
2026-07-19 21:47:48 -0500

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -604,8 +604,12 @@ app.get('/api/servicios', async (req, res) => {
             query += ' AND s.fecha_servicio = ?';
             params.push(req.query.fecha);
         }
+        if (search) {
+            query += ` AND (u1.nombre LIKE ? OR s.nombre_jugador_temp LIKE ?)`;
+            params.push(`%${search}%`, `%${search}%`);
+        }
 
-        query += ' ORDER BY s.fecha_servicio DESC, s.hora_inicio_programada ASC LIMIT 1000';
+        query += ' ORDER BY s.fecha_servicio DESC, s.hora_inicio_programada ASC, u3.nombre ASC LIMIT 1000';
         
         const servicios = await db.prepare(query).all(...params);
         res.json(servicios);
